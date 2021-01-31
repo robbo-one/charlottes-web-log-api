58dbaa14c23b6c29cfc2a378532adfba458903f0
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-02-01 12:33:09 +1300

    added post route

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,23 @@
-const config = require('./knexfile').development
-const db = require('knex')(config)
+const { post } = require('../routes/posts')
+const connection = require('./connection')
+
+function getPosts(db = connection) {
+  return db('posts')
+}
+
+function addPosts(post, db = connection) {
+  return db('posts')
+    .insert(post)
+}
+
+function getPostId(id, db = connection) {
+  return db('posts')
+    .where('posts.id', id)
+    .first()
+}
 
 module.exports = {
+  getPosts,
+  addPosts,
+  getPostId
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,34 @@
 const express = require('express')
+const router = express.Router()
+const camelCase = require('camelcase-keys')
 
 const db = require('../db/db')
 
-const router = express.Router()
-
 // put routes here
+
+router.get('/', (req,res) => {
+  db.getPosts()
+    .then(posts => {
+      const postData = posts.map(post => {
+        post.paragraphs = JSON.parse(post.paragraphs)
+        return post
+      })
+      res.json(camelCase(postData))
+    })
+})
+
+router.post('/', (req,res) => {
+  const post = req.body
+  post.paragraphs = JSON.stringify(post.paragraphs)
+  post.date_created = new Date()
+  db.addPosts(post)
+    .then(id => {
+      db.getPostId(id[0])
+      .then(newPost => {
+          console.log(newPost)
+          res.json(camelCase(newPost))
+        })
+    })
+})
 
 module.exports = router
