6df69b187efdf402ae61ae7fa7dcfa05de76946e
student <student@eda-bc-09.localdomain>
2021-02-01 12:32:59 +1300

    did some of the things

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,28 @@
 const config = require('./knexfile').development
-const db = require('knex')(config)
+const connection = require('knex')(config)
+const camelcaseKeys = require('camelcase-keys');
 
 module.exports = {
+  getBlogs: getBlogs,
+  postNewBlog: postNewBlog,
+  updateExisitingBlog: updateExisitingBlog
+
 }
+
+
+
+function getBlogs (db = connection) {
+  return db('Posts').select()
+}
+
+function postNewBlog (post, db = connection) {
+  return db('Posts').insert(post)
+  .then(ids => ids[0])
+}
+
+function updateExisitingBlog (post, db = connection){
+  return db('Posts').patch(post)
+  .then(ids => ids[0])
+}
+
+
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,53 @@
+const { json } = require('express')
 const express = require('express')
+const camelcaseKeys = require('camelcase-keys');
 
-const db = require('../db/db')
+const db = require('../db/db');
+// const { camelCase } = require('../db/db');
 
 const router = express.Router()
 
 // put routes here
+
+router.get('/', (req, res) => {
+  db.getBlogs()
+  .then(Posts => {
+    const postData = Posts.map((post)=>{
+      post.paragraphs = JSON.parse(post.paragraphs)
+      return post
+    })
+    res.json(camelcaseKeys (postData)) 
+  }
+  )
+})
+
+router.post('/', (req, res) => {
+  const post = req.body
+  console.log(post)
+  db.postNewBlog(post)
+  .then(id => {
+    res.json({ id: id })
+  })
+})
+
+// router.get('/:id', (req, res) => {
+
+// })
+
+router.post('/:id', (req, res) => {
+  const post = req.body
+  console.log(post)
+  db.updateExisitingBlog(post)
+  .then(Posts => {
+    const updateData = Posts.find((post) => {
+    post.id = JSON.parse(post.paragraphs)
+    return post
+  })
+    res.json(camelcaseKeys(updateData))
+})
+})
+
+
+
 
 module.exports = router
