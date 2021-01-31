ba9c5eb88f9cb4ccd3aeeb53adb2914233115e31
Ste-Perry <73620112+Ste-Perry@users.noreply.github.com>
2021-02-01 11:35:37 +1300

    add blogs

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,17 @@
 const config = require('./knexfile').development
-const db = require('knex')(config)
+const connection = require('knex')(config)
+
+
+ function getBlog (db = connection) {
+    return db('posts').select()
+}
+
+function addBlog (post, db = connection){
+  return db('posts').insert(post)
+    .then(id => ids[0])
+}
 
 module.exports = {
+    getBlog: getBlog,
+    addBlog: addBlog
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,4 +6,20 @@ const router = express.Router()
 
 // put routes here
 
+router.get('/', (req,res) => {
+    db.getBlog()
+    .then(posts =>
+        res.json(posts))
+})
+
+router.post('/', (req,res) => {
+    const post = req.body
+    console.log(post)
+    db.addBlog(post)
+    .then(id => {
+        res.json({id: id})
+    })
+
+})
+
 module.exports = router
