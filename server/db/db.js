67778e9e9e3a2ea4f4f2761e31f11a6f2c1ebb7b
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-02-01 10:59:13 +1300

    get posts request done

--- server/db/db.js
+++ server/db/db.js
@@ -2,4 +2,10 @@ const config = require('./knexfile').development
 const db = require('knex')(config)
 
 module.exports = {
+  getPosts: getPosts
+}
+
+function getPosts () {
+  return db('posts')
+    .select()
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const camelcaseKeys = require('camelcase-keys')
 
 const db = require('../db/db')
 
 const router = express.Router()
 
 // put routes here
+router.get('/', (req, res) => {
+  db.getPosts()
+    .then(posts => {     
+      res.json(camelcaseKeys(posts))
+    })
+})
 
 module.exports = router
