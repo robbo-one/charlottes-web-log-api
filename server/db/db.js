d20fa8530e124ffa4180f553f98dc4056934b1e7
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-02-01 11:08:35 +1300

    first route up and running v1/posts

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,10 @@
 const config = require('./knexfile').development
 const db = require('knex')(config)
 
+function getBlogPosts () {
+    return db('posts').select()
+}
+
 module.exports = {
+    getBlogPosts
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,5 +5,12 @@ const db = require('../db/db')
 const router = express.Router()
 
 // put routes here
+router.get('/', (req, res) => {
+    db.getBlogPosts()
+    .then(posts => {
+        console.log(posts)
+        res.json(posts)
+    })
+})
 
 module.exports = router
