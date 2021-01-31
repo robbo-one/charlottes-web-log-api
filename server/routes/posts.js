1b66f37e4c7ee8a60a0a45b076e3d7c88738d662
student <student@eda-bc-11.localdomain>
2021-02-01 10:42:04 +1300

    added base routes and libraries for handling camel and snakecase

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,4 +6,36 @@ const router = express.Router()
 
 // put routes here
 
+router.get('/v1/posts', (req, res)=> {
+  res.json()
+})
+
+router.post('/v1/posts', (req, res)=> {
+  res.json()
+})
+
+router.patch('/v1/posts/:id', (req, res)=> {
+  res.json()
+})
+
+router.delete('/v1/posts/:id', (req, res)=> {
+  res.json()
+})
+
+router.get('/v1/posts/:postId/comments', (req, res)=> {
+  res.json()
+})
+
+router.post('/v1/posts/:postId/comments', (req, res)=> {
+  res.json()
+})
+
+router.patch('/v1/comments/:commentId', (req, res)=> {
+  res.json()
+})
+
+router.delete('/v1/comments/:commentId', (req, res)=> {
+  res.json()
+})
+
 module.exports = router
