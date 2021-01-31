615f5162531ef7ad90852296ed583a9e8a696734
student <student@eda-bc-08.localdomain>
2021-02-01 11:28:03 +1300

    Get Posts route in place

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,23 @@
-const config = require('./knexfile').development
-const db = require('knex')(config)
+//This is where the database functions are written
 
-module.exports = {
+const config = require('./knexfile').development
+const connection = require('knex')(config)
+
+module.exports = { getPosts
 }
+
+//A function to get a list of blog posts.  Will return an array of objects.
+
+function getPosts (db = connection) {
+  return db('posts').select()
+    .then(posts => {
+      return posts.map(post => {
+        post.dateCreated = post.date_created
+        delete post.date_created
+        return post
+      })
+    })
+}
+
+
+
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,26 @@
+//This is where API functions are written
+
 const express = require('express')
-
 const db = require('../db/db')
-
 const router = express.Router()
 
 // put routes here
+// REST JSON API Index Route GET /posts
+//  /v1/posts
+
+router.get('/', (req,res) => {
+  db.getPosts()
+    .then(posts => {
+      // res.send(JSON.stringify(posts))
+      res.json(posts)
+    })
+})
+
+
+
+
+
+
+
 
 module.exports = router
