a298acc25ee07967ad42103a90120a30cd5477f3
student <student@eda-bc-02.localdomain>
2021-02-01 16:46:21 +1300

    nearly finish make mew post objective

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,27 @@
-const config = require('./knexfile').development
-const db = require('knex')(config)
+// const config = require('./knexfile').development
+// const db = require('knex')(config)
+const connection = require('./connection')
+
+function getPosts(db = connection) {
+    return db('posts').select()
+    .then(posts => {
+        return posts.map(post => {
+            post.paragraphs = JSON.parse(post.paragraphs)
+            return post
+        })
+
+    })
+}
+    
+
+function addPost(post, db = connection) {
+    post.paragraphs = JSON.stringify(post.paragraphs)
+    return db('posts').insert(post)
+    .then(ids => ids[0])
+}
+
 
 module.exports = {
+    getPosts,
+    addPost
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,29 @@
+const { json } = require('express')
 const express = require('express')
+const { get } = require('lodash')
 
 const db = require('../db/db')
 
 const router = express.Router()
 
 // put routes here
+
+router.get('/', (req, res) => {
+    db.getPosts()
+        .then(posts => {
+            res.json(posts)
+        })
+})
+
+router.post('/', (req,res) => {
+    const post = req.body
+    db.addPost(post)
+        .then(id => {
+            db.getPostById(id)
+            .then(post => {
+                res.json(post)
+            })
+        })
+})
 
 module.exports = router
